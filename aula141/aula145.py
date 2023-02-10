7b6f5d9c5e91bba8c0289f7086ef18e8cac887c1
Carolina Felix <carolrigaudfelix@gmail.com>
2023-02-10 18:08:32 -0300

    polimorfismo e exceptions

---
+++ aula141/aula145.py
@@ -0,0 +1,14 @@
+"""
+Criando Exceptions em Python Orientando a Objetos
+Para criar uma Exception em Python, você só
+precisa herdar de alguma exceção da linguagem.
+A recomendação da doc é herdar de Exception.
+Criando exceções (comum colocar Error ao final)
+Levantando (raise) / Lançando (throw) exceções
+Relançando exceções
+Adicionando notas em exceção (3.11.0)
+"""
+class MeuError(Exception):
+    ...
+    
+
