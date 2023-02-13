36f821c8138a011fc4bbad7e6124c2ba4fa56a20
Carolina Felix <carolrigaudfelix@gmail.com>
2023-02-13 15:22:02 -0300

    tratando exceptions

--- aula141/aula145.py
+++ aula141/aula145.py
@@ -12,3 +12,22 @@ class MeuError(Exception):
     ...
     
 
+class OutroError(Exception):
+    ...
+
+def levantar():
+    exception_ = MeuError('a', 'b', 'c')
+    exception_.add_note('Olha a nota 1')
+    exception_.add_note('Vc errou isso')
+    raise exception_
+
+try:
+    levantar()
+except (MeuError, ZeroDivisionError) as error:
+    print(error.__class__.__name__)
+    print(error.args)
+    exception_ = OutroError('Vou lançar de novo')
+    exception_.__notes__ = error.__notes__.copy()
+    exception_.add_note('Mais uma nota')
+    raise exception_ from error
+
