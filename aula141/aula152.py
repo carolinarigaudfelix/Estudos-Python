b08b0a8023f337debdcdefbdd364a1c74d7bb4d4
Carolina Felix <carolrigaudfelix@gmail.com>
2023-02-14 14:14:12 -0300

    classes decoradoras

---
+++ aula141/aula152.py
@@ -0,0 +1,17 @@
+#Classes decoradoras (Decorator classes)
+class Multiplicar:
+    def __init__(self, multiplicador):
+        self._multiplicador = multiplicador
+
+    def __call__(self, func):
+        def interna(*args, **kwargs):
+            resultado = func(*args, **kwargs)
+            return resultado * self._multiplicador
+        return interna
+
+@Multiplicar(10) #letra maiuscula pq é classe
+def soma(x, y):
+    return x * y
+
+dois_mais_dois_mais_quatro = soma(2,4)
+print(dois_mais_dois_mais_quatro)
