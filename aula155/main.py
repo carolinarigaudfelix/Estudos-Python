957177698e035d1eff51464f01b04863040beea8
Carolina Felix <carolrigaudfelix@gmail.com>
2023-02-15 14:46:52 -0300

    DocStrings

---
+++ aula155/main.py
@@ -0,0 +1,10 @@
+import uma_linha, varias_linhas, aula156
+
+# print(dir(uma_linha)) #diz tudo que tem dentro do módulo
+# print(uma_linha.__doc__) # da pra acessar essas coisas do dir
+# print(uma_linha.__file__)
+# print(uma_linha.__name__)
+
+#help(uma_linha) #descreve td q tem no modulo, mt útil
+help(aula156)
+
