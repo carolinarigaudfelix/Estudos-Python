84371bf2072efd4e8f2cdd6cc0b1d0b7a3c4b33c
Carolina Felix <carolrigaudfelix@gmail.com>
2023-02-21 17:10:49 -0300

    secrets and random

--- modulo-4/aula180.py
+++ modulo-4/aula180.py
@@ -1,3 +1,15 @@
+# secrets gera números aleatórios seguros
+import secrets
+import string as s
+from secrets import SystemRandom as sr
+
+print(''.join (sr().choices(s.ascii_letters + s.digits + s.punctuation, k=10))) 
+#gera uma senha aleatoria
+
+random = secrets.SystemRandom()
+
+#print(secrets.randbelow(100))
+
 # random tem geradores de números pseudoaleatórios
 # Obs.: números pseudoaleatórios significa que os números
 # parecem ser aleatórios, mas na verdade não são. Portanto,
@@ -8,34 +20,34 @@
 import random
 
 # Funções:
-# seed
+# seed          NÃO FAZ nada
 #   -> Inicializa o gerador de random (por isso "números pseudoaleatórios")
-# random.seed(0)
+#random.seed(0)
 
 # random.randrange(início, fim, passo)
 #   -> Gera um número inteiro aleatório dentro de um intervalo específico
-r_range = random.randrange(10, 20, 2) #pega de 10 a 20 pulando de 2 em 2
+# r_range = random.randrange(10, 20, 2) #pega de 10 a 20 pulando de 2 em 2
 # print(r_range)
 
 # random.randint(início, fim)
 #   -> Gera um número inteiro aleatório dentro de um intervalo "sem passo"
-r_int = random.randint(10, 20)
+# r_int = random.randint(10, 20)
 # print(r_int)
 
 # random.uniform(início, fim)
 #   -> Gera um número flutuante aleatório dentro de um intervalo "sem passo"
-r_uniform = random.uniform(10, 20) 
+# r_uniform = random.uniform(10, 20) 
 # print(r_uniform)
 
 # random.shuffle(SequenciaMutável) -> Embaralha a lista original
 nomes = ['Luiz', 'Maria', 'Helena', 'Joana']
-# random.shuffle(nomes)
-# print(nomes)
+random.shuffle(nomes)
+print(nomes)
 
 # random.sample(Iterável, k=N)
 #   -> Escolhe elementos do iterável e retorna outro iterável (não repete)
-novos_nomes = random.sample(nomes, k=3) #k é o tamanho do sample, pega 2 
-#elementos aleatório de 2 itens da lista e armazena na variável
+# novos_nomes = random.sample(nomes, k=3) #k é o tamanho do sample, pega 2 
+# #elementos aleatório de 2 itens da lista e armazena na variável
 # print(nomes)
 # print(novos_nomes)
 
