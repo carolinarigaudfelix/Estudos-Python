635f1eb347ec7b265d7628bb4c7ec0e266365610
Carolina Felix <carolrigaudfelix@gmail.com>
2023-02-21 15:29:02 -0300

    json

---
+++ modulo-4/aula174.py
@@ -0,0 +1,43 @@
+# json.dumps e json.loads com strings + typing.TypedDict
+# Ao converter de Python para JSON:
+# Python        JSON
+# dict          object
+# list, tuple   array
+# str           string
+# int, float    number
+# True          true
+# False         false
+# None          null
+import json
+#from pprint import pprint
+from typing import TypedDict #habilita q vc cria uma classe para tipar o dict
+
+class Movie(TypedDict):
+    title:str
+    original_title: str
+    is_movie: bool
+    imdb_rating: float
+    year: int
+    characters: list[str]
+    budget: None | float
+
+string_json = ''' 
+
+{
+  "title": "O Senhor dos Anéis: A Sociedade do Anel",
+  "original_title": "The Lord of the Rings: The Fellowship of the Ring",
+  "is_movie": true,
+  "imdb_rating": 8.8,
+  "year": 2001,
+  "characters": ["Frodo", "Sam", "Gandalf", "Legolas", "Boromir"],
+  "budget": null
+}
+'''
+
+filme: Movie = json.loads(string_json)
+# pprint(filme, width=40)
+# print(filme['title'])
+# print(filme['characters'][0])
+# print(filme['year'] +10)
+json_string = json.dumps(filme, ensure_ascii=False, indent=2) #ensure_ascii coloca a acentuação
+print(json_string) #n da pra pegar coisas dai mas da pra por isso em um arquivo
