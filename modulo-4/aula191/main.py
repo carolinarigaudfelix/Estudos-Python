5fc480bc654581db7bca7b7df2712e276a2093f0
Carolina Felix <carolrigaudfelix@gmail.com>
2023-02-25 16:42:55 -0300

    selenium and jupyter

---
+++ modulo-4/aula191/main.py
@@ -0,0 +1,77 @@
+from pathlib import Path
+from time import sleep
+
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys #tem várias teclas importantes
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+# Chrome Options
+# https://peter.sh/experiments/chromium-command-line-switches/
+# Doc Selenium
+# https://selenium-python.readthedocs.io/locating-elements.html
+
+
+
+# Caminho para a raiz do projeto
+ROOT_FOLDER = Path(__file__).parent
+# Caminho para a pasta onde o chromedriver está
+CHROME_DRIVER_PATH = ROOT_FOLDER / 'drivers' / 'chromedriver'
+
+
+def make_chrome_browser(*options: str) -> webdriver.Chrome:
+    chrome_options = webdriver.ChromeOptions()
+
+    # chrome_options.add_argument('--headless')
+    if options is not None:
+        for option in options:
+            chrome_options.add_argument(option)  # type: ignore
+
+    chrome_service = Service(
+        executable_path=str(CHROME_DRIVER_PATH),
+    )
+
+    browser = webdriver.Chrome(
+        service=chrome_service, #fala o serviço q vai utilizar o chrome drive
+        options=chrome_options 
+    )
+
+    return browser
+
+
+if __name__ == '__main__':
+    TIME_TO_WAIT = 10
+
+    # Example
+    # options = '--headless', '--disable-gpu',
+    options = ()
+    browser = make_chrome_browser(*options)
+
+    # Como antes
+    browser.get('https://www.google.com')
+
+    # Espere para encontrar o input
+    search_input = WebDriverWait(browser, TIME_TO_WAIT).until( #procura até
+        #a sentença for verdadeira
+        EC.presence_of_element_located(
+            (By.NAME, 'q')
+        )
+    )
+
+
+    search_input.send_keys('Hello World!')
+    search_input.send_keys(Keys.ENTER) #dps do hello world ele pressiona enter
+    
+    #ao invés de usar search_input pra esperar, se vc sabe 
+    # q vai aparecer na tela
+
+    results = browser.find_element(By.ID, 'search')
+    print(results) #dentro desse results vc pode pegar coisas
+    links = results.find_elements(By.TAG_NAME, 'a')
+    links[0].click()
+
+
+    # Dorme por 10 segundos
+    sleep(TIME_TO_WAIT)
